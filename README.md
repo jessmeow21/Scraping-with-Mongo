1cce6764badc66a4da55d4891c5796609e12edae
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-09-24 13:43:36 -0400

    created readme and server file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Scraping-with-Mongo
+
+# The-Mongo-Scraper
+<!-- 
+1. First on the command line do: npm init
+2. This creates the package json
+3. Next npm install the packages required to run program
+4. Require these packages in the server.js 
+5. Do a gitignore file for node modules because that folder is too big and not nessesary to push to github
+6.
+
+ -->
+## Technologies Included:
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* Mongo DB
+* Cherrio js https://github.com/cheeriojs/cheerio
+Node NPM Packages:
+* Express npm i express
+* Express-handlebars npm i express-handlebars
+* mongo js npm i mongojs
+* mongoose npm i mongoose
+* cheerio npm i cheerio
+* axios npm i axios
+* npm i body-parser
+* gitignore for node modules
+* morgan npm i morgan - HTTP request logger middleware for node.js
+* request - simplified way to make HTTP calls npm i request
---
+++ server.js
@@ -0,0 +1,61 @@
+//npm i express
+//npm i body-parser
+//npm i express-handlebars
+//npm i morgan
+//npm i mongoose
+//npm i axios
+//npm i cheerio
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs  = require('express-handlebars');
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Initialize express-handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+//require api and html routes files
+require("./routes/index")(app)
+
+
+
+
+
+
+
+
+
+//test connection
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function(){
+//     console.log("Connected to Mongoose!");
+// });
+
+// Start the server
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
